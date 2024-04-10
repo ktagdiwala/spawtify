4a43b06561cf5b5ea428d0feb6eb899727e03571
James Mondragon <jaimondragon@csumb.edu>
2024-04-09 20:22:47 -0700

    SpawtifyDatabase moved to main folder and added DB_NAME as well as abstract method getUserDAO

--- app/src/main/java/com/example/spawtify/Database/SpawtifyDatabase.java
+++ app/src/main/java/com/example/spawtify/Database/SpawtifyDatabase.java
@@ -3,10 +3,12 @@ package com.example.spawtify.Database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-import com.example.spawtify.Database.entities.Songlist;
 import com.example.spawtify.Database.entities.User;
 
 @Database(entities = {User.class}, version = 1, exportSchema = false)
 public abstract class SpawtifyDatabase extends RoomDatabase {
+    public static final String DB_NAME = "Spawtify_Database";
     public static final String USER_TABLE = "User_Table";
+
+    public abstract UserDAO getUserDAO();
 }
