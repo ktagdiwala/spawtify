c92c5d41a9e965a9bcba123ee80620ab1c2b58ba
James Mondragon <jaimondragon@csumb.edu>
2024-04-09 21:04:50 -0700

    MainActivity: intentFactory created

--- app/src/main/java/com/example/spawtify/MainActivity.java
+++ app/src/main/java/com/example/spawtify/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.spawtify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.spawtify.Database.UserDAO;
@@ -12,6 +14,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     //  USER FIELDS
+    private static final String USER_ID_KEY = "com.example.spawtify.userIdKey";
     private UserDAO userDao;
     private List<User> users;
     private int userId = -1;
@@ -20,5 +23,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static Intent intentFactory(Context context, int userId){
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(USER_ID_KEY, userId);
+        return intent;
     }
 }
