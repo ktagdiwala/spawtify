2458af8f54d0324cdf00a2310f5fc62877e03af0
James Mondragon <jaimondragon@csumb.edu>
2024-04-09 20:43:39 -0700

    LoginActivity: getDatabase done, wireUpDisplay mostly done

--- app/src/main/java/com/example/spawtify/LoginActivity.java
+++ app/src/main/java/com/example/spawtify/LoginActivity.java
@@ -1,15 +1,51 @@
 package com.example.spawtify;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import com.example.spawtify.Database.SpawtifyDatabase;
+import com.example.spawtify.Database.UserDAO;
+import com.example.spawtify.Database.entities.User;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText username;
+    private EditText password;
+    private Button button;
 
+    private UserDAO userDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        wireUpDisplay();
+        getDatabase();
+
+    }
+
+    private void getDatabase() {
+        userDAO = Room.databaseBuilder(this, SpawtifyDatabase.class, SpawtifyDatabase.DB_NAME)
+                .allowMainThreadQueries()
+                .build()
+                .getUserDAO();
+    }
+
+    private void wireUpDisplay(){
+        username = findViewById(R.id.editTextLoginUsername);
+        password = findViewById(R.id.editTextLoginPassword);
+        button = findViewById(R.id.buttonLogin);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
