13b0343f9a1cbe88ebe059f4f533808ff0bb7ca7
James Mondragon <jaimondragon@csumb.edu>
2024-04-10 17:17:08 -0700

    SongDAO: created, moved to main spawtify folder

---
+++ app/src/main/java/com/example/spawtify/Database/SongDAO.java
@@ -0,0 +1,27 @@
+package com.example.spawtify.Database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.spawtify.Database.entities.Song;
+
+import java.util.ArrayList;
+
+/** SongDAO:
+ * DAO object associated with a song
+ * @author Krishna Tagdiwala
+ * @since 04-09-2024
+ */
+
+@Dao
+public interface SongDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Song song);
+
+    @Query("SELECT * FROM " + SpawtifyDatabase.SONGLIST)
+    ArrayList<Song> getAllRecords();
+
+
+}
