cdbe6b6c2c4bf77d7889b4850bb5b0bc30417f58
ktagdiwala <krishna.tagdiwala@gmail.com>
2024-04-13 23:41:25 -0700

    BrowseSongsActivity.java: added ActivityBinding and recycler view

--- app/src/main/java/com/example/spawtify/BrowseSongsActivity.java
+++ app/src/main/java/com/example/spawtify/BrowseSongsActivity.java
@@ -4,32 +4,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.spawtify.databinding.ActivityBrowseSongsBinding;
+import com.example.spawtify.viewHolders.SonglistAdapter;
+
+/** BrowseSongsActivity:
+ * Display for user to view the current song list
+ * User can also filter songs by artist/album/genre/explicit
+ * @author Krishna Tagdiwala
+ * @since 04-13-2024
+ */
 public class BrowseSongsActivity extends AppCompatActivity {
 
-    /** BrowseSongsActivity:
-     * Display for user to view the current song list
-     * User can also filter songs by artist/album/genre/explicit
-     * @author Krishna Tagdiwala
-     * @since 04-13-2024
-     */
+    ActivityBrowseSongsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_browse_songs);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityBrowseSongsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        RecyclerView recyclerView = binding.SongDisplayRecyclerView;
+        final SonglistAdapter adapter = new SonglistAdapter(new SonglistAdapter.SongDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
 
 
     public static Intent intentFactory(Context context){
