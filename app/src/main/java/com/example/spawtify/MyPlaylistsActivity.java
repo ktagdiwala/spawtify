6e8772a4b38864be147d2a88c9388c4ae8d99ee2
ktagdiwala <krishna.tagdiwala@gmail.com>
2024-04-15 23:55:35 -0700

    MyPlaylistsActivity.java: Created intentFactory and started setting up display

--- app/src/main/java/com/example/spawtify/MyPlaylistsActivity.java
+++ app/src/main/java/com/example/spawtify/MyPlaylistsActivity.java
@@ -1,24 +1,50 @@
 package com.example.spawtify;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.spawtify.Database.SpawtifyRepository;
+import com.example.spawtify.Database.entities.User;
+import com.example.spawtify.databinding.ActivityMyPlaylistsBinding;
 
 public class MyPlaylistsActivity extends AppCompatActivity {
+
+    // allows to read information from the display
+    ActivityMyPlaylistsBinding binding;
+
+    //  Used as a key to retrieve value (int: userId) from MainActivity
+    private static final String USER_ID_KEY = "com.example.spawtify.userIdKey";
+
+    // Creates a user object, initialized in MainActivity
+    private User user;
+
+    // Stores current userId
+    private int userId;
+
+    //Creates SpawtifyRepository object initialized in OnCreate
+    private SpawtifyRepository spawtifyRepository;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_my_playlists);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMyPlaylistsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        user.setUserId(Integer.parseInt(USER_ID_KEY));
+
+        spawtifyRepository = SpawtifyRepository.getRepository(getApplication());
+//        String setTitleString =  user.getUsername() + "'s Playlists";
+//        binding.MyPlaylistsTextview.setText(setTitleString);
+        binding.MyPlaylistsDescriptionTextview.setText(R.string.click_on_a_playlist_to_view_contained_songs);
+    }
+
+    public static Intent intentFactory(Context context, int userId) {
+        Intent intent = new Intent(context, MyPlaylistsActivity.class);
+        intent.putExtra(USER_ID_KEY, userId);
+        return intent;
     }
 }
