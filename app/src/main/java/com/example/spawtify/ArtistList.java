df96c5e324c10210b4a60c2f04377c127a39e3f9
James Mondragon <jaimondragon@csumb.edu>
2024-04-16 14:22:00 -0700

    ArtistList: Activity that displays list of artists, still need to implement onClick functionality

---
+++ app/src/main/java/com/example/spawtify/ArtistList.java
@@ -0,0 +1,51 @@
+package com.example.spawtify;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.spawtify.Database.SpawtifyRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ArtistList extends AppCompatActivity {
+
+    //  Holds our distinct artists retrieved from database
+    List<String> artists = new ArrayList<>();
+
+    //  Used to interact with database
+    SpawtifyRepository spawtifyRepository;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist_list);
+
+        RecyclerView recyclerView = findViewById(R.id.artistRecyclerView);
+
+        spawtifyRepository = SpawtifyRepository.getRepository(getApplication());
+
+        //  Sets title of activity in top left of action bar
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Filter by Artist");
+
+        setUpArtistList();
+
+        ArtistAdapter adapter = new ArtistAdapter(this, artists);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void setUpArtistList() {
+        artists = spawtifyRepository.getDistinctArtists();
+    }
+
+    public static Intent intentFactory(Context context){
+        Intent intent = new Intent(context, ArtistList.class);
+        return intent;
+    }
+}
