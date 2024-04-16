796d0d29def6e75f60a494b985dacf8cd1c1972e
James Mondragon <jaimondragon@csumb.edu>
2024-04-16 14:24:04 -0700

    FilterSongs: Created, set up artist filter button, still need to set up the other filter buttons

---
+++ app/src/main/java/com/example/spawtify/FilterSongs.java
@@ -0,0 +1,42 @@
+package com.example.spawtify;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spawtify.databinding.ActivityBrowseSongsBinding;
+import com.example.spawtify.databinding.ActivityFilterSongsBinding;
+
+import java.util.Objects;
+import java.util.logging.Filter;
+
+public class FilterSongs extends AppCompatActivity {
+
+    ActivityFilterSongsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityFilterSongsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        wireUpDisplay();
+    }
+
+    private void wireUpDisplay(){
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Filter Songs");
+
+        binding.artistFilterButton.setOnClickListener(v -> artistList());
+    }
+
+    private void artistList() {
+        Intent intent = ArtistList.intentFactory(this);
+        startActivity(intent);
+    }
+
+    public static Intent intentFactory(Context context){
+        Intent intent = new Intent(context, FilterSongs.class);
+        return intent;
+    }
+}
