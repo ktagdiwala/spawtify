bc17743c03e6acacab24a8959112022457f63c7b
James Mondragon <jaimondragon@csumb.edu>
2024-04-16 17:23:35 -0700

    ArtistAdapter: removed some unused imports, unrelated to album filter work

--- app/src/main/java/com/example/spawtify/ArtistAdapter.java
+++ app/src/main/java/com/example/spawtify/ArtistAdapter.java
@@ -1,8 +1,6 @@
 package com.example.spawtify;
 
 import android.content.Context;
-import android.content.ReceiverCallNotAllowedException;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
