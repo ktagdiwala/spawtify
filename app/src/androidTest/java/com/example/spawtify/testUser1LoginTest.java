39e872bd8c3613994e474ad9ef9e0b572d2750ab
James Mondragon <jaimondragon@csumb.edu>
2024-04-17 16:01:10 -0700

    testUser1LoginTest: Test file that simulates testuser1 logging in, confirms successful login by checking that logoutMenuItem exists and has the username displayed on it

---
+++ app/src/androidTest/java/com/example/spawtify/testUser1LoginTest.java
@@ -0,0 +1,103 @@
+package com.example.spawtify;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class testUser1LoginTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testUser1LoginTest() {
+        //  Enters "testuser1" into editTextLoginUsername EditText
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.editTextLoginUsername),
+                        childAtPosition(
+                                allOf(withId(R.id.main),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("testuser1"), closeSoftKeyboard());
+
+        //  Enters "testuser1" into editTextLoginPassword EditText
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.editTextLoginPassword),
+                        childAtPosition(
+                                allOf(withId(R.id.main),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("testuser1"), closeSoftKeyboard());
+
+        //  Performs click on buttonLogin button
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.buttonLogin), withText("LOGIN"),
+                        childAtPosition(
+                                allOf(withId(R.id.main),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                3),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        //  Checks that Logout Menu Item a) exists b) has text matching our username "testuser1"
+        ViewInteraction button = onView(
+                allOf(withId(R.id.logoutMenuItem), withText("TESTUSER1"),
+                        withParent(withParent(withId(com.google.android.material.R.id.action_bar))),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
